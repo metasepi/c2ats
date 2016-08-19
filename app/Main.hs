721b9ca9aa90b4bf4b021ac6dd457e0e6687ed96
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-08-19 17:07:18 +0900

    Add sub command

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,11 @@ import Language.C2ATS
 
 main :: IO ()
 main = do
-  let usage = error "Usage: c2ats C_FILEPATH"
+  let usage = error "Usage: c2ats new C_FILEPATH"
   args <- getArgs
-  when (length args /= 1) usage
-  let [fn] = args
+  when (length args /= 2) usage
+  let cmd:[fn] = args
+  when (cmd /= "new") usage
   (files, globals) <- parseMyFile fn
   let fglobal = sortFlatGlobal . flatGlobal $ globals
   -- xxx Pick up typedef using pointer
