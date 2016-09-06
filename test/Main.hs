e0e6680ac528a1349e49f8793c302f479020a2da
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-09-06 18:06:19 +0900

    Start to write unit test

---
+++ test/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Test.Framework.TH
+import Test.Framework
+import Test.HUnit
+import Test.Framework.Providers.HUnit
+
+import Language.C2ATS
+
+main :: IO ()
+main = return ()
