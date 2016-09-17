fbabdbf51d5a3c3d72e517cdda53933151cbb4f6
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-09-17 11:03:48 +0900

    Use Data.Set

--- src/Language/C2ATS/Process.hs
+++ src/Language/C2ATS/Process.hs
@@ -10,6 +10,8 @@ module Language.C2ATS.Process
 
 import Data.Maybe
 import Data.List
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Debug.Trace {- for warnings -}
@@ -73,19 +75,19 @@ injectIncludes noincs m =
 
 --------------------------------------------------------------------------------
 sortFlatGlobal :: [FlatG] -> [FlatG]
-sortFlatGlobal = (\(a,_,_,b) -> reverse a ++ b) . foldl go ([], Map.empty, Map.empty, []) . sortBy order
+sortFlatGlobal = (\(a,_,_,b) -> reverse a ++ b) . foldl go ([], Set.empty, Set.empty, []) . sortBy order
   where
     order :: FlatG -> FlatG -> Ordering
     order (_, a) (_, b) = nodeInfo a `compare` nodeInfo b
-    go :: ([FlatG], Map Int (), Map Int (), [FlatG]) -> FlatG ->
-          ([FlatG], Map Int (), Map Int (), [FlatG])
+    go :: ([FlatG], Set Int, Set Int, [FlatG]) -> FlatG ->
+          ([FlatG], Set Int, Set Int, [FlatG])
     go (out, knowns, deps, ks) fg@(s,_) =
-      let knowns' = Map.insert (nodeSUERef s) () knowns
-          deps'   = Map.difference (Map.union deps $ anons fg) knowns'
-          out'    = (if Map.null deps' then reverse ks ++ [fg] else []) ++ out
-          ks'     = if Map.null deps' then [] else fg : ks
+      let knowns' = Set.insert (nodeSUERef s) knowns
+          deps'   = Set.difference (Set.union deps $ anons fg) knowns'
+          out'    = (if Set.null deps' then reverse ks ++ [fg] else []) ++ out
+          ks'     = if Set.null deps' then [] else fg : ks
       in (out', knowns', deps', ks')
-    anons :: FlatG -> Map Int ()
+    anons :: FlatG -> Set Int
     anons (_, g) = anonRefs g
 
 nodeSUERef :: SUERef -> Int
@@ -94,7 +96,7 @@ nodeSUERef (NamedRef (Ident _ _ (NodeInfo _ _ n))) = nameId n
 nodeSUERef _                                       = -1
 
 class AnonRefs p where
-  anonRefs :: p -> Map Int ()
+  anonRefs :: p -> Set Int
 
 instance AnonRefs FlatGlobalDecl where
   anonRefs (FGObj  d) = anonRefs d
@@ -105,7 +107,7 @@ instance AnonRefs IdentDecl where
   anonRefs (Declaration (Decl v _))     = anonRefs v
   anonRefs (ObjectDef (ObjDef v _ _))   = anonRefs v
   anonRefs (FunctionDef (FunDef v _ _)) = anonRefs v
-  anonRefs (EnumeratorDef _)            = Map.empty -- ATS does not have enum
+  anonRefs (EnumeratorDef _)            = Set.empty -- ATS does not have enum
 
 instance AnonRefs MemberDecl where
   anonRefs (MemberDecl v _ _)   = anonRefs v
@@ -115,48 +117,48 @@ instance AnonRefs VarDecl where
   anonRefs (VarDecl _ _ t) = anonRefs t
 
 instance AnonRefs TagDef where
-  anonRefs (EnumDef _)                    = Map.empty  -- ATS does not have enum
-  anonRefs (CompDef (CompType _ _ m _ _)) = Map.unions $ map anonRefs m
+  anonRefs (EnumDef _)                    = Set.empty  -- ATS does not have enum
+  anonRefs (CompDef (CompType _ _ m _ _)) = Set.unions $ map anonRefs m
 
 instance AnonRefs TypeDef where
   anonRefs (TypeDef _ t _ _) = anonRefs t
 
 instance AnonRefs Type where
-  anonRefs (PtrType t _ _)                             = anonRefs t
-  anonRefs (ArrayType t _ _ _)                         = anonRefs t
-  anonRefs (FunctionType ft _)                         = anonRefs ft
-  anonRefs (DirectType tn _ _)                         = anonRefs tn
-  anonRefs _                                           = Map.empty
+  anonRefs (PtrType t _ _)     = anonRefs t
+  anonRefs (ArrayType t _ _ _) = anonRefs t
+  anonRefs (FunctionType ft _) = anonRefs ft
+  anonRefs (DirectType tn _ _) = anonRefs tn
+  anonRefs _                   = Set.empty
 
 instance AnonRefs TypeName where
   anonRefs (TyComp (CompTypeRef s _ _)) = anonRefs s
-  anonRefs _                            = Map.empty
+  anonRefs _                            = Set.empty
 
 instance AnonRefs FunType where
   anonRefs (FunTypeIncomplete t) = anonRefs t
   anonRefs (FunType t p _)       =
-    Map.union (anonRefs t) (Map.unions $ map anonRefs p)
+    Set.union (anonRefs t) (Set.unions $ map anonRefs p)
 
 instance AnonRefs ParamDecl where
   anonRefs (ParamDecl v _)         = anonRefs v
   anonRefs (AbstractParamDecl v _) = anonRefs v
 
 instance AnonRefs SUERef where
-  anonRefs s@(AnonymousRef _) = Map.singleton (nodeSUERef s) ()
-  anonRefs (NamedRef _)       = Map.empty
+  anonRefs s@(AnonymousRef _) = Set.singleton $ nodeSUERef s
+  anonRefs (NamedRef _)       = Set.empty
 
 --------------------------------------------------------------------------------
-type IndentsMap = (Maybe String, Map String ())
+type IndentsMap = (Maybe String, Set String)
 
 injectForwardDecl :: [FlatG] -> [FlatG]
-injectForwardDecl = reverse . fst . foldl f ([], Map.empty)
+injectForwardDecl = reverse . fst . foldl f ([], Set.empty)
   where
-    f :: ([FlatG], Map String ()) -> FlatG -> ([FlatG], Map String ())
+    f :: ([FlatG], Set String) -> FlatG -> ([FlatG], Set String)
     f (fgs, is) fg@(s,g) =
       let (i, is') = idents g
-          knownis  = maybe is (\a -> Map.insert a () is) i
-          fds      = forwardDecls $ Map.keys $ Map.difference is' knownis
-          is''     = Map.union is' knownis
+          knownis  = maybe is (\a -> Set.insert a is) i
+          fds      = forwardDecls $ Set.elems $ Set.difference is' knownis
+          is''     = Set.union is' knownis
       in (fg : fds ++ fgs, is'')
 
 forwardDecls :: [String] -> [FlatG]
@@ -165,8 +167,8 @@ forwardDecls = map f
     f i = (NamedRef $ mkIdent nopos i (Name (-1)), FGRaw $ "abst@ype " ++ i ++ " // FIXME! Forward declaration.")
 
 identsAppend :: IndentsMap -> IndentsMap -> IndentsMap
-identsAppend (i_a@(Just _), is_a) (i_b, is_b) = (i_a, Map.union is_a is_b)
-identsAppend (Nothing, is_a) (i_b, is_b)      = (i_b, Map.union is_a is_b)
+identsAppend (i_a@(Just _), is_a) (i_b, is_b) = (i_a, Set.union is_a is_b)
+identsAppend (Nothing, is_a) (i_b, is_b)      = (i_b, Set.union is_a is_b)
 
 class Idents p where
   idents :: p -> IndentsMap
@@ -175,13 +177,13 @@ instance Idents FlatGlobalDecl where
   idents (FGObj  d) = idents d
   idents (FGTag  d) = idents d
   idents (FGType d) = idents d
-  idents (FGRaw  _) = (Nothing, Map.empty)
+  idents (FGRaw  _) = (Nothing, Set.empty)
 
 instance Idents IdentDecl where
   idents (Declaration (Decl v _))             = idents v
   idents (ObjectDef (ObjDef v _ _))           = idents v
   idents (FunctionDef (FunDef v _ _))         = idents v
-  idents _                                    = (Nothing, Map.empty)
+  idents _                                    = (Nothing, Set.empty)
 
 instance Idents MemberDecl where
   idents (MemberDecl v _ _)   = idents v
@@ -192,24 +194,24 @@ instance Idents VarDecl where
 
 instance Idents TagDef where
   idents (CompDef (CompType (NamedRef i) k m _ _)) =
-    foldl (\a b -> a `identsAppend` idents b) (Just $ show k ++ "_c2ats_" ++ identToString i, Map.empty) m
+    foldl (\a b -> a `identsAppend` idents b) (Just $ show k ++ "_c2ats_" ++ identToString i, Set.empty) m
   idents (CompDef (CompType (AnonymousRef _) _ m _ _)) =
-    foldl (\a b -> a `identsAppend` idents b) (Nothing, Map.empty) m
-  idents _                              = (Nothing, Map.empty)
+    foldl (\a b -> a `identsAppend` idents b) (Nothing, Set.empty) m
+  idents _                              = (Nothing, Set.empty)
 
 instance Idents TypeDef where
   idents (TypeDef i t _ _) =
-    (Just $ "type_c2ats_" ++ identToString i, Map.empty) `identsAppend` idents t
+    (Just $ "type_c2ats_" ++ identToString i, Set.empty) `identsAppend` idents t
 
 instance Idents Type where
   idents (PtrType t _ _)                               = idents t
   idents (ArrayType t _ _ _)                           = idents t
   idents (FunctionType ft _)                           = idents ft
   idents (DirectType (TyComp (CompTypeRef (NamedRef i) k _)) _ _) =
-    (Nothing, Map.singleton (show k ++ "_c2ats_" ++ identToString i) ())
+    (Nothing, Set.singleton $ show k ++ "_c2ats_" ++ identToString i)
   idents (TypeDefType (TypeDefRef i _ _) _ _)          =
-    (Nothing, Map.singleton ("type_c2ats_" ++ identToString i) ())
-  idents _                                             = (Nothing, Map.empty)
+    (Nothing, Set.singleton $ "type_c2ats_" ++ identToString i)
+  idents _                                             = (Nothing, Set.empty)
 
 instance Idents FunType where
   idents (FunTypeIncomplete t) = idents t
