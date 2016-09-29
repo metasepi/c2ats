f6c77b6c0b17648984ede3b521d0b1e603e83c86
Kiwamu Okabe <kiwamu@debian.or.jp>
2016-09-29 13:30:29 +0900

    Write global decls into sats tree

--- src/Language/C2ATS/HeaderTree.hs
+++ src/Language/C2ATS/HeaderTree.hs
@@ -12,6 +12,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy.Char8 as BLC
+import qualified Data.ByteString.Internal as BI
 import Control.Monad
 import Control.Exception
 import System.FilePath
@@ -20,6 +21,7 @@ import System.Process
 import System.Exit
 
 import Language.C2ATS.FlatGlobalDecl
+import Language.C2ATS.Pretty
 
 data CHeader = CHeaderQuot FilePath
              | CHeaderLess FilePath
@@ -61,7 +63,7 @@ includeHeaders mapHead hPath buf =
     handler :: MapCHeader -> SomeException -> IO (MapCHeader, [CHTree])
     handler mapHead _ = return (mapHead, [])
     incs :: B.ByteString -> [B.ByteString]
-    incs = filter (BC.isPrefixOf "#include") . BC.lines
+    incs = filter (BC.isPrefixOf "#include") . map (B.filter (/= BI.c2w ' ')) . BC.lines
     toCHeader :: B.ByteString -> CHeader
     toCHeader inc =
       if isJust $ BC.find (== '"') inc
@@ -92,37 +94,47 @@ headerTree gcc copts file = do
   (rFile, buf) <- readFileHeader hPath file'
   toTree hPath Map.empty rFile file' buf
 
-traverseTree :: Monad m => (Tree a -> m ()) -> Tree a -> m ()
-traverseTree f t@(Node {rootLabel = r, subForest = []}) = f t
-traverseTree f t@(Node {rootLabel = r, subForest = s})  =
-  f t >> mapM_ (traverseTree f) s
+foldMTree :: Monad m => (b -> Tree a -> m b) -> b -> Tree a -> m b
+foldMTree f b t@(Node {rootLabel = r, subForest = []}) = f b t
+foldMTree f b t@(Node {rootLabel = r, subForest = s})  =
+  f b t >>= (\a -> foldMTree' f a s)
+
+foldMTree' :: Monad m => (b -> Tree a -> m b) -> b -> [Tree a] -> m b
+foldMTree' f b []     = return b
+foldMTree' f b (t:ts) = foldMTree f b t >>= (\a -> foldMTree' f a ts)
 
 createSATS :: FilePath -> MapCHeader -> CHTree -> Map (Maybe FilePath) [FlatG] -> IO ()
-createSATS oDir mapHead cTrees sGlobal = traverseTree go cTrees
+createSATS oDir mapHead cTrees sGlobal =
+  foldMTree go sGlobal cTrees >>= (\a -> print $ Map.keys a)
   where
-    go :: CHTree -> IO ()
-    go (Node {rootLabel = (CHeaderQuot file, rPath), subForest = sub}) =
-      go' file rPath sub (\f -> unlines [
-                             "%{#",
-                             "#include \"" ++ f ++ "\"",
-                             "%}"
-                             ])
-    go (Node {rootLabel = (CHeaderLess file, rPath), subForest = sub}) =
-      go' file rPath sub (\f -> unlines [
-                             "%{#",
-                             "#include <" ++ f ++ ">",
-                             "%}"
-                             ])
-    go' :: FilePath -> FilePath -> [CHTree] -> (String -> String) -> IO ()
-    go' file rPath sub doInc = do
-      let rPath' = oDir </> tail rPath
-      isNotExist <- fmap not $ doesFileExist $ rPath' -<.> ".sats"
+    go :: Map (Maybe FilePath) [FlatG] -> CHTree -> IO (Map (Maybe FilePath) [FlatG])
+    go sg (Node {rootLabel = (CHeaderQuot file, rPath), subForest = sub}) =
+      go' sg rPath sub (unlines [
+                           "%{#",
+                           "#include \"" ++ file ++ "\"",
+                           "%}"
+                           ])
+    go sg (Node {rootLabel = (CHeaderLess file, rPath), subForest = sub}) =
+      go' sg rPath sub (unlines [
+                           "%{#",
+                           "#include <" ++ file ++ ">",
+                           "%}"
+                           ])
+    go' :: Map (Maybe FilePath) [FlatG] -> FilePath -> [CHTree] -> String
+           -> IO (Map (Maybe FilePath) [FlatG])
+    go' sg rPath sub inc = do
+      let sats   = oDir </> tail rPath -<.> ".sats"
+      isNotExist <- fmap not $ doesFileExist $ sats
       when isNotExist $ do
-        createDirectoryIfMissing True $ takeDirectory $ rPath'
-        writeFile (rPath' -<.> ".sats") $ "// File: " ++ rPath -<.> ".sats\n"
+        createDirectoryIfMissing True $ takeDirectory sats
+        writeFile sats $ "// File: " ++ rPath -<.> ".sats\n"
         when (not . null $ sub) $
-          appendFile (rPath' -<.> ".sats") $ foldr stainc "" sub
-        appendFile (rPath' -<.> ".sats") $ doInc file
+          appendFile sats $ foldr stainc "" sub
+        appendFile sats inc
+        let insg = Map.lookup (Just rPath) sg
+        when (isJust insg) $
+          appendFile sats $ show . atsPrettyGlobal $ fromJust insg
+      return $ Map.delete (Just rPath) sg
     stainc :: CHTree -> String -> String
     stainc (Node {rootLabel = (_, rPath), subForest = _}) =
       (++) $ "#include \"" ++ "{$C2ATS}" </> tail rPath -<.> ".sats" ++ "\"\n"
